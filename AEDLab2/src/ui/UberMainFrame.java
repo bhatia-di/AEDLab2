374f3dc298977fbdebf5d9e5483613230c90278a
bhatia-di <bhatia.di@northeastern.edu>
2021-10-02 10:55:17 -0400

    table added

--- AEDLab2/src/ui/UberMainFrame.java
+++ AEDLab2/src/ui/UberMainFrame.java
@@ -29,19 +29,79 @@ public class UberMainFrame extends javax.swing.JFrame {
 
         UberMainTabbedFrame = new javax.swing.JTabbedPane();
         adminPanel = new javax.swing.JPanel();
+        carCatalogHeaderLabel = new javax.swing.JLabel();
+        carCatalogScrollPane = new javax.swing.JScrollPane();
+        carCatalogTable = new javax.swing.JTable();
+        viewButton = new javax.swing.JButton();
+        deleteButton = new javax.swing.JButton();
         searchPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        UberMainTabbedFrame.setForeground(new java.awt.Color(0, 0, 102));
+        UberMainTabbedFrame.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        UberMainTabbedFrame.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+
+        carCatalogHeaderLabel.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        carCatalogHeaderLabel.setForeground(new java.awt.Color(0, 0, 102));
+        carCatalogHeaderLabel.setText("Car Catalogue");
+
+        carCatalogTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        carCatalogScrollPane.setViewportView(carCatalogTable);
+
+        viewButton.setText("View ");
+        viewButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                viewButtonActionPerformed(evt);
+            }
+        });
+
+        deleteButton.setText("Delete");
+        deleteButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deleteButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout adminPanelLayout = new javax.swing.GroupLayout(adminPanel);
         adminPanel.setLayout(adminPanelLayout);
         adminPanelLayout.setHorizontalGroup(
             adminPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1249, Short.MAX_VALUE)
+            .addGroup(adminPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(adminPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(carCatalogScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 1215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(carCatalogHeaderLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(28, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, adminPanelLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(viewButton)
+                .addGap(18, 18, 18)
+                .addComponent(deleteButton)
+                .addGap(56, 56, 56))
         );
         adminPanelLayout.setVerticalGroup(
             adminPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 690, Short.MAX_VALUE)
+            .addGroup(adminPanelLayout.createSequentialGroup()
+                .addGap(9, 9, 9)
+                .addComponent(carCatalogHeaderLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(carCatalogScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(adminPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(viewButton)
+                    .addComponent(deleteButton))
+                .addGap(403, 403, 403))
         );
 
         UberMainTabbedFrame.addTab("Admin", adminPanel);
@@ -72,6 +132,14 @@ public class UberMainFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void viewButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_viewButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_viewButtonActionPerformed
+
+    private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_deleteButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -111,6 +179,11 @@ public class UberMainFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTabbedPane UberMainTabbedFrame;
     private javax.swing.JPanel adminPanel;
+    private javax.swing.JLabel carCatalogHeaderLabel;
+    private javax.swing.JScrollPane carCatalogScrollPane;
+    private javax.swing.JTable carCatalogTable;
+    private javax.swing.JButton deleteButton;
     private javax.swing.JPanel searchPanel;
+    private javax.swing.JButton viewButton;
     // End of variables declaration//GEN-END:variables
 }
